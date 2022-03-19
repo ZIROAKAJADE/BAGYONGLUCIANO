a22a5fe06f56ecd91b6270c09fb9d32e0c07f7d7
ZIROAKAJADE <101941912+ZIROAKAJADE@users.noreply.github.com>
2022-03-20 00:44:03 +0800

    Update luciano.py

--- luciano.py
+++ luciano.py
@@ -19,7 +19,6 @@ def start():
       s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       s.connect((ip,port))
       s.send(hh)
-    pass
     for i in range(pack):
       s.send(hh)
     xx += 1
